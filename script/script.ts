8207b3fa47db0fcc5a3e5def0e164da9264ff686
Camila Zucchi <camilazucchi@outlook.com>
2023-09-27 17:36:46 -0300

    feat: add typescript

--- script/script.ts
+++ script/script.ts
@@ -1,16 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const hours = document.getElementById("hours");
-    const minutes = document.getElementById("minutes");
-    const seconds = document.getElementById("seconds");
+    const hours = document.getElementById("hours") as HTMLSpanElement;
+    const minutes = document.getElementById("minutes") as HTMLSpanElement;
+    const seconds = document.getElementById("seconds") as HTMLSpanElement;
+
     function updateTime() {
         const dateToday = new Date();
         hours.textContent = formatTime(dateToday.getHours());
         minutes.textContent = formatTime(dateToday.getMinutes());
         seconds.textContent = formatTime(dateToday.getSeconds());
     }
-    function formatTime(time) {
+
+    function formatTime(time: number): string {
         return String(time).padStart(2, '0');
     }
+
     setInterval(updateTime, 1000);
     updateTime();
-});
+
+})
